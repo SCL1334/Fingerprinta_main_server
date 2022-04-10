9a8b243c6388a922f7e512f92c6ae953396b8e8d
SCL1334 <violetlan1122@gmail.com>
2022-04-10 14:25:20 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+const {
+  PORT, API_VERSION,
+} = process.env;
+
+// Express Initialization
+const express = require('express');
+
+const app = express();
+
+app.set('json spaces', 2);
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// server test
+app.get('/', (req, res) => {
+  res.send('OK');
+});
+
+// Error handling
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send('Internal Server Error');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
