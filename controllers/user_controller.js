d51451d2f99a93a439d1255917e8df74532bb197
SCL1334 <violetlan1122@gmail.com>
2022-04-13 01:27:51 +0800

    Fix: make API input/output format consistent

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -3,7 +3,7 @@ const Fingerprint = require('../models/fingerprint_model');
 
 const createAccount = async (req, res) => {
   const {
-    name, account, password, classId,
+    name, account, password, class_id,
   } = req.body;
   let { role } = req.body;
   if (!role) return res.status(400).json({ error: 'Invalid input' });
@@ -17,7 +17,7 @@ const createAccount = async (req, res) => {
   } else {
     return res.status(400).json({ error: 'Invalid input' });
   }
-  const result = await User.createAccount(name, account, password, classId, roleId);
+  const result = await User.createAccount(name, account, password, class_id, roleId);
   if (result === 0) {
     return res.status(500).json({ error: 'Create failed' });
   }
@@ -28,18 +28,16 @@ const createAccount = async (req, res) => {
 };
 
 const getAccounts = async (req, res) => {
-  const [accounts] = await User.getAccounts();
-  if (accounts === 0) {
-    res.status(500).json({ error: 'Read failed' });
-  } else if (accounts === -1) {
-    res.status(400).json({ error: 'Read failed due to invalid input' });
+  const accounts = await User.getAccounts();
+  if (accounts) {
+    res.status(200).json({ data: accounts });
   } else {
-    res.status(200).json({ data: { accounts } });
+    res.status(500).json({ error: 'Read failed' });
   }
 };
 
 const deleteAccount = async (req, res) => {
-  const { userId } = req.body;
+  const userId = req.body.user_id;
   const result = await User.deleteAccount(userId);
   if (result === 0) {
     res.status(500).json({ error: 'Delete failed' });
