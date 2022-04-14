278db2bd860f31e1ccb6d40c2709aea419be59c3
SCL1334 <violetlan1122@gmail.com>
2022-04-14 21:32:26 +0800

    Update: userAPI to student and staff

--- routes/user_route.js
+++ routes/user_route.js
@@ -2,15 +2,32 @@ const router = require('express').Router();
 
 const { wrapAsync } = require('../util/util');
 const {
-  createAccount, getAccounts, deleteAccount, signIn, signOut, getProfile, matchFingerprint,
+  createStudent,
+  getStudents,
+  deleteStudent,
+  createStaff,
+  getStaffs,
+  deleteStaff,
+  studentSignIn,
+  staffSignIn,
+  signOut,
+  getProfile,
+  matchFingerprint,
 } = require('../controllers/user_controller');
 
-router.route('/users/signin').post(wrapAsync(signIn));
-router.route('/users/signout').post(wrapAsync(signOut));
-router.route('/users/profile').get(wrapAsync(getProfile));
-router.route('/users/fingerprints').post(wrapAsync(matchFingerprint));
-router.route('/users/accounts').post(wrapAsync(createAccount));
-router.route('/users/accounts').get(wrapAsync(getAccounts));
-router.route('/users/accounts/:id').delete(wrapAsync(deleteAccount));
+router.route('students/profile').get(wrapAsync(getProfile));
+router.route('/students/:id/fingerprint').post(wrapAsync(matchFingerprint));
+
+router.route('/students/signin').post(wrapAsync(studentSignIn));
+router.route('/students').post(wrapAsync(createStudent));
+router.route('/students').get(wrapAsync(getStudents));
+router.route('/students/:id').delete(wrapAsync(deleteStudent));
+
+router.route('/staffs/signin').post(wrapAsync(staffSignIn));
+router.route('/staffs').post(wrapAsync(createStaff));
+router.route('/staffs').get(wrapAsync(getStaffs));
+router.route('/staffs/:id').delete(wrapAsync(deleteStaff));
+// clear session
+router.route('/signout').post(wrapAsync(signOut));
 
 module.exports = router;
