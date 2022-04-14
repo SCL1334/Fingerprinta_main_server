100d69d5b59e689d7ca1d18b3e5caebf20a1ce95
SCL1334 <violetlan1122@gmail.com>
2022-04-14 17:16:46 +0800

    Update: fit db design devide user to student / staff

--- test/fake_data_generator.js
+++ test/fake_data_generator.js
@@ -5,36 +5,40 @@ const dayjs = require('dayjs');
 const salt = parseInt(process.env.BCRYPT_SALT, 10);
 const { promisePool } = require('../models/mysqlcon');
 
-// role, name, account, password, class_id, finger_id
-const createFakeUser = async (num, role, classId = null) => {
+// name, email, password, class_id, finger_id
+const createFakeUser = async (num, roleId, classId = null) => {
   try {
-    let roleId;
-    if (role.toLowerCase() === 'student') {
-      roleId = 2;
-    } else if (role.toLowerCase() === 'teacher') {
-      roleId = 1;
-    }
+    const role = { 1: 'student', 2: 'teacher' };
     const password = 'test';
     // gen fake data
     const users = [];
     for (let i = 0; i < num; i += 1) {
-      const name = `test_${role}_${i + 1}`;
-      const account = `${name}@test.com`;
+      const name = `test_${role[roleId]}_${i + 1}`;
+      const email = `${name}@test.com`;
       const hashedPassword = await bcrypt.hash(password, salt);
-      users.push([roleId, name, account, hashedPassword, classId]);
+      if (classId) {
+        users.push([name, email, hashedPassword, classId]);
+      } else if (classId === null) {
+        users.push([name, email, hashedPassword]);
+      }
     }
     // save fake data
-    await promisePool.query('INSERT INTO user (role, name, account, password, class_id) VALUES ?', [users]);
+    if (roleId === 1) {
+      await promisePool.query('INSERT INTO student (name, email, password, class_id) VALUES ?', [users]);
+    } else if (roleId === 2) {
+      await promisePool.query('INSERT INTO staff (name, email, password) VALUES ?', [users]);
+    }
     console.log('completed');
   } catch (err) {
     console.log(err);
   }
 };
 
+// 1: 'student', 2: 'teacher'
 // gen 15 students first, same class
-// createFakeUser(15, 'student', 1);
-// gen 1 teacher
-// createFakeUser(1, 'teacher');
+// createFakeUser(15, 1, 1);
+// gen 5 teacher
+// createFakeUser(5, 2);
 
 const truncatePunch = async () => {
   await promisePool.query('TRUNCATE student_punch;');
@@ -44,11 +48,11 @@ const truncatePunch = async () => {
 // student_id, punch_in, punch_out
 const createFakePunch = async (days) => {
   try {
-    // await truncatePunch();
+    await truncatePunch();
     const today = dayjs().format('YYYY-MM-DD');
     const punchInInit = dayjs(`${today} 09:00:00`);
     const punchOutInit = dayjs(`${today} 18:00:00`);
-    const [students] = await promisePool.query('SELECT id FROM user WHERE role = 2');
+    const [students] = await promisePool.query('SELECT id FROM student');
     const randomMin = () => Math.floor(Math.random() * 60) - 30;
     const attendance = [];
     for (let i = days - 1; i >= 0; i -= 1) {
