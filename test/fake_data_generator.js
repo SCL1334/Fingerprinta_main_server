58cf61b48e1359ad1732b466af4fea691b491938
SCL1334 <violetlan1122@gmail.com>
2022-04-16 23:33:01 +0800

    Add: fake_leave_application

--- test/fake_data_generator.js
+++ test/fake_data_generator.js
@@ -5,6 +5,11 @@ const dayjs = require('dayjs');
 const salt = parseInt(process.env.BCRYPT_SALT, 10);
 const { promisePool } = require('../models/mysqlcon');
 
+const truncateTable = async (tableName) => {
+  await promisePool.query(`TRUNCATE ${tableName};`);
+  console.log('truncated');
+};
+
 // name, email, password, class_id, finger_id
 const createFakeUser = async (roleId, startId, num, classId = null) => {
   try {
@@ -40,15 +45,10 @@ const createFakeUser = async (roleId, startId, num, classId = null) => {
 // gen 5 teacher
 // createFakeUser(2, 1, 5);
 
-const truncatePunch = async () => {
-  await promisePool.query('TRUNCATE student_punch;');
-  console.log('completed');
-};
-
 // student_id, punch_in, punch_out
 const createFakePunch = async (days) => {
   try {
-    await truncatePunch();
+    await truncateTable('student_punch');
     const today = dayjs();
     const punchInInit = dayjs(`${today.format('YYYY-MM-DD')} 09:00:00`);
     const punchOutInit = dayjs(`${today.format('YYYY-MM-DD')} 18:00:00`);
@@ -72,4 +72,51 @@ const createFakePunch = async (days) => {
 };
 
 // each student punch in past 30 days
-createFakePunch(30);
+// createFakePunch(30);
+
+const createLeaveTypes = async () => {
+  try {
+    await truncateTable('leave_type');
+    const leaveTypes = [['personal'], ['sick']];
+    await promisePool.query('INSERT INTO leave_type (name) VALUES ?', [leaveTypes]);
+    console.log('completed');
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const createFakeLeaveApplications = async (days, rate) => {
+  try {
+    await truncateTable('student_leave');
+    const today = dayjs();
+    const startInit = dayjs(`${today.format('YYYY-MM-DD')} 09:00:00`);
+    const endInit = dayjs(`${today.format('YYYY-MM-DD')} 18:00:00`);
+    const [students] = await promisePool.query('SELECT id FROM student');
+    const [types] = await promisePool.query('SELECT id FROM leave_type');
+    const random = (num) => Math.floor(Math.random() * num);
+    const randomHour = () => Math.floor(Math.random() * 5);
+    const description = 'test';
+    const applications = [];
+
+    for (let i = days - 1; i >= 0; i -= 1) {
+      const date = today.subtract(i, 'day');
+      students.forEach((student) => {
+        if (random(rate) !== 0) {
+          return;
+        }
+        const start = startInit.add(randomHour(), 'hour');
+        const end = endInit.subtract(randomHour(), 'hour');
+        const type = types[random(types.length)].id;
+        applications.push([student.id, type, description, date.format('YYYY-MM-DD'), start.format('HH:mm:ss'), end.format('HH:mm:ss')]);
+      });
+    }
+    console.log(applications);
+    await promisePool.query('INSERT INTO student_leave (student_id, leave_type_id, description, date, start, end) VALUES ?', [applications]);
+    console.log('completed');
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// createLeaveTypes();
+createFakeLeaveApplications(15, 15);
