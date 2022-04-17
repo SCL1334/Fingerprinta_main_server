aaced61f1cef8ca7de607e6b7f0dd204f37316fe
SCL1334 <violetlan1122@gmail.com>
2022-04-17 22:54:18 +0800

    Add: staff check student attendance, class and student filter

---
+++ public/js/staff.js
@@ -0,0 +1,108 @@
+$(document).ready(async () => {
+  const leaveTypeTable = { 1: '事假', 2: '病假' };
+  const leaveStatusTable = { 0: '審核中', 1: '已審核' };
+  try {
+    // init page, check if valid signin
+    const profile = await axios.get('/api/1.0/staffs/profile');
+    if (profile.error) { throw new Error('aioxs fail'); }
+    const { data } = await profile;
+    const {
+      id, name, email, classes,
+    } = data.data;
+    $('.userName').text(`你好 ${data.data.name}`);
+
+    // sign out btn
+    $('.signout').click(async () => {
+      try {
+        const responseData = await axios.post('/api/1.0/signout');
+        const { data } = await responseData;
+        if (data) {
+          location.href = location.href.replace('.html', '_signin.html');
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    });
+
+    // get attendance
+    $('.get_attendances').click(async () => {
+      $('.content').text('');
+
+      const attendance = $('<div></div>').attr('class', 'attendance').text('出勤查詢');
+
+      const searchFrom = $('<input>').attr('type', 'date').attr('class', 'search_from');
+      const searchTo = $('<input>').attr('type', 'date').attr('class', 'search_to');
+      const searchBtn = $('<button></button>').attr('class', 'search_btn').text('查詢');
+      const classOptions = $('<select></select>').attr('class', 'class_options');
+      const classInitOption = $('<option value=0>全部班級</option>');
+      classOptions.append(classInitOption);
+      const studentOptions = $('<select></select>').attr('class', 'students_options');
+      const studentInitOption = $('<option value=0>全部學生</option>');
+      studentOptions.append(studentInitOption);
+
+      // get init class options
+      try {
+        const classDetail = await axios.get('/api/1.0/classes');
+        const classData = classDetail.data;
+        classData.data.forEach((clas) => {
+          classOptions.append(`<option value=${clas.id}>${clas.batch}</option>`);
+        });
+      } catch (err) {
+        console.log(err);
+      }
+
+      classOptions.change(async () => {
+        try {
+          studentOptions.empty();
+          if ($('.class_options').val() === '0') {
+            studentOptions.append(studentInitOption);
+            return;
+          }
+          const studentDetail = await axios.get(`/api/1.0/classes/${$('.class_options').val()}/students`);
+          const studentData = studentDetail.data;
+          studentData.data.forEach((student) => {
+            studentOptions.append(`<option value=${student.id}>${student.name}</option>`);
+          });
+        } catch (err) {
+          console.log(err);
+        }
+      });
+
+      attendance.append(classOptions, studentOptions, searchFrom, searchTo, searchBtn);
+      $('.content').append(attendance);
+      $('.search_btn').click(async () => {
+        try {
+          let table = $('.attendance_result');
+          if (table) { table.text(''); }
+          const from = ($('.search_from').val()) ? `?from=${$('.search_from').val()}`.replaceAll('-', '') : '';
+          const to = $('.search_to').val() ? `&to=${$('.search_to').val()}`.replaceAll('-', '') : '';
+          const responseData = await axios.get(`/api/1.0/students/${id}/attendances${from}${to}`);
+          const { data } = responseData;
+          table = $('<table></table>').attr('class', 'attendance_result');
+          const tr = $('<tr></tr>');
+          const heads = ['打卡日期', '上課打卡', '下課打卡'];
+          heads.forEach((head) => {
+            const th = $('<th></th>').text(head);
+            tr.append(th);
+          });
+          table.append(tr);
+
+          $('.attendance').append(table);
+          data.data.forEach((attendance) => {
+            const tr = $('<tr></tr>');
+            const td_date = $('<td></td>').text(attendance.punch_date);
+            const td_punch_in = $('<td></td>').text(attendance.punch_in);
+            const td_punch_out = $('<td></td>').text(attendance.punch_out);
+            tr.append(td_date, td_punch_in, td_punch_out);
+            table.append(tr);
+          });
+        } catch (err) {
+          console.log(err);
+        }
+      });
+    });
+  } catch (err) {
+    console.log(err);
+    location.href = location.href.replace('.html', '_signin.html');
+  }
+});
