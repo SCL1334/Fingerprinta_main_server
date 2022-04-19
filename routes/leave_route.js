65a68fb27173d6633ea8e236e7edf3175b26ebf8
SCL1334 <violetlan1122@gmail.com>
2022-04-19 15:09:23 +0800

    Update: move get a student leaves path

--- routes/leave_route.js
+++ routes/leave_route.js
@@ -8,7 +8,6 @@ const {
 router.route('/leaves/types').get(wrapAsync(getTypes));
 router.route('/leaves/types').post(wrapAsync(createType));
 router.route('/leaves/types/:id').delete(wrapAsync(deleteType));
-router.route('/leaves').get(wrapAsync(getAllLeaves));
 router.route('/leaves/:id').put(wrapAsync(approveLeave));
 router.route('/leaves/:id').delete(wrapAsync(deleteLeave));
 
