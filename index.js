a654b99891b4b268156211d6251ab236bfba0586
SCL1334 <violetlan1122@gmail.com>
2022-05-18 09:55:31 +0800

    Add: 404 not found

--- index.js
+++ index.js
@@ -52,10 +52,20 @@ app.get('/', (req, res) => {
   res.send('OK');
 });
 
+// 404 error
+app.use('/api/1.0', (req, res, next) => {
+  res.status(404).json({ error: { message: 'Sorry cant find that!' } });
+});
+
+// for page site
+app.use((req, res, next) => {
+  res.status(301).redirect('/not_found.html');
+});
+
 // Error handling
 app.use((err, req, res, next) => {
   console.log(err);
-  res.status(500).send('Internal Server Error');
+  res.status(500).json({ error: { message: 'Internal Server Error' } });
 });
 
 app.listen(PORT, async () => {
